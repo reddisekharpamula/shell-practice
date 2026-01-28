9874789068bd33e6a83864ad1a03f1eb67cc32dd
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-28 19:36:29 +0530

    Shell Scripting

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -9,3 +9,4 @@ echo "The sum of $NUM1 and $NUM2 is: $SUM"
 FRUITS=("Apple" "Banana" "Cherry" "Date")
 echo "Fruits in the list are: ${FRUITS[@]}"
 echo "First fruit in the list is ${FRUITS[0]}"
+echo "Total number of fruits: ${#FRUITS[@]}"
