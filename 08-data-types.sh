8fcb434460327286a80532b3aef08a3bcebccc6d
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-28 19:32:06 +0530

    Shell Scripting

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -3,3 +3,9 @@ NUM1=10
 NUM2=20
 SUM=$((NUM1 + NUM2))
 echo "The sum of $NUM1 and $NUM2 is: $SUM"
+
+#List example
+
+FRUITS=("Apple" "Banana" "Cherry" "Date")
+echo "Fruits in the list are: ${FRUITS[@]}"
+echo "First fruit in the list is ${FRUITS[0]}"
