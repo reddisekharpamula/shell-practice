3126b7f5ab9a0830e5c6ee582ec0aa5e10ce8990
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-28 17:49:19 +0530

    Shell Scripting

---
+++ 03-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Jithisha" # there wont be space between variable and =
+PERSON2="Rishi"
+echo "$PERSON1: Hello $PERSON2, how are you today?"
+echo "$PERSON2: Hi $PERSON1, I'm doing well, thank you! How about you?"
+echo "$PERSON1: I'm great, thanks for asking! Have you finished the project we were working on?"
+echo "$PERSON2: Yes, I completed it last night. Do you want me to send it over?"
+echo "$PERSON1: That would be perfect. Please send it to my email."
+echo "$PERSON2: Will do! I'll send it right away."
