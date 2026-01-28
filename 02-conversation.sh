b6de58e85dd98bb47d3c8a393c7a7cb43dfa0cab
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-28 17:33:47 +0530

    Shell Scripting

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Jithisha: Hello Rishi, how are you today?"
+echo "Rishi: Hi Jithisha, I'm doing well, thank you! How about you?"
+echo "Jithisha: I'm great, thanks for asking! Have you finished the project we were working on?"
+echo "Rishi: Yes, I completed it last night. Do you want me to send it over?"
+echo "Jithisha: That would be perfect. Please send it to my email."
+echo "Rishi: Will do! I'll send it right away."
