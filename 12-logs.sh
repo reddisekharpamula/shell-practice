b18edf4c7478731b48033d367e3deb85ac6128e3
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-31 06:14:36 +0530

    Shell Scripting

---
+++ 12-logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-scripts"
+LOGS_FILE="/var/logs/shell-scripts/$0.log"
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+
+VALIDATION()
+{
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+VALIDATION $? "Installing Nginx" >>& $LOGS_FILE
+
+
+dnf install mysql -y
+
+VALIDATION $? "Installing MySQL" >>& $LOGS_FILE
+
+
+dnf install nodejs -y
+
+VALIDATION $? "Installing nodejs" >>& $LOGS_FILE
