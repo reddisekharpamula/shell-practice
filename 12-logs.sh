d2a288c53ebf33bcc1197790c79b38e4e829701f
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-01-31 06:27:20 +0530

    Shell Scripting

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,35 +1,31 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-scripts"
-LOGS_FILE="/var/log/shell-scripts/$0.log"
-mkdir -p $LOGS_FOLDER
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access" |& tee -a $LOGS_FILE
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
+mkdir -p $LOGS_FOLDER
 
-VALIDATION()
-{
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE" |& tee -a $LOGS_FILE
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS" |& tee -a $LOGS_FILE
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
-dnf install nginx -y
-VALIDATION $? "Installing Nginx" &>> $LOGS_FILE
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
 
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql"
 
-dnf install mysql -y
-
-VALIDATION $? "Installing MySQL" &>> $LOGS_FILE
-
-
-dnf install nodejs -y
-
-VALIDATION $? "Installing nodejs" &>> $LOGS_FILE
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
